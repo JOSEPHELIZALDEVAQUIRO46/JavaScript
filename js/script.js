443f356bf59b2110389565952e8a619dedd8d7d8
JOSEPHELIZALDEVAQUIRO46 <joseph_elizalde@soy.sena.edu.co>
2023-07-24 22:22:40 -0500

    Se creo unas operaciones

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//document.write("analisis y desarrollo de software")
+//var edad=33
+//console.log("Mi edad es:"+edad);
+
+/*const num1=2
+const num2=3
+const div = num1/num2
+parseInt (div)
+alert("la div es: "+div)*/
+
+let nam=prompt("escriba el nombre")
+let ape=prompt("Escriba su apellido")
+let edad=prompt("Escriba su edad")
+let pes=prompt("Escriba su peso")
+let dir=prompt("Escriba su direccion")
+alert("mi nombre es " +nam+ " " +ape+ " tengo " +edad+ " años, vivo en " +dir+ " y peso " +pes+ "kg")
+/*"mostrar una alerta con los nombres y apellidos completos, edad, peso y direccion utilizando la funcion promt"*/
